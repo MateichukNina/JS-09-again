2afe1256c043702c6e174f2c6240851de15c125b
MateichukNina <124276133+MateichukNina@users.noreply.github.com>
2024-03-12 18:00:48 +0200

    color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,33 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.body;
+
+let intervalId;
+
+function colorSwitcher() {
+  startBtn.addEventListener('click', onStart);
+  stopBtn.addEventListener('click', onStop);
+
+  function onStart() {
+    const randomColor = getRandomHexColor();
+    body.style.backgroundColor = randomColor;
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    intervalId = setInterval(() => {
+      const randomColor = getRandomHexColor();
+      body.style.background = randomColor;
+    }, 1000);
+  }
+
+  function onStop() {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    clearInterval(intervalId);
+  }
+}
+colorSwitcher();
