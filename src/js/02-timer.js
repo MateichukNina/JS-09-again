7c545ab39449d45439730db39a85042922cc6e68
MateichukNina <124276133+MateichukNina@users.noreply.github.com>
2024-03-13 23:45:42 +0200

    timer finaly

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,16 +1,15 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
-import Notiflix from "notiflix";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
-
-const picker = document.querySelector("input#datetime-picker");
-const startBtn = document.querySelector("button[data-start]");
+const picker = document.querySelector('input#datetime-picker');
+const startBtn = document.querySelector('button[data-start]');
 const dataDays = document.querySelector('[data-days]');
 const dataHours = document.querySelector('[data-hours]');
 const dataMinutes = document.querySelector('[data-minutes]');
 const dataSeconds = document.querySelector('[data-seconds]');
 
-
+startBtn.disabled = true;
 
 const options = {
   enableTime: true,
@@ -19,15 +18,24 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     console.log(selectedDates[0]);
+    const currentDate = new Date();
+    const selectedDate = new Date(selectedDates[0]);
+    if (selectedDate <= currentDate) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      startBtn.disabled = true;
+    } else {
+      startBtn.disabled = false;
+    }
   },
 };
+
+
 
 const dataPicker = flatpickr(picker, options);
 let targetDate;
 let intervalId;
 
 function convertMs(ms) {
-  
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
@@ -47,38 +55,34 @@ function formatTime(value) {
   return value.toString().padStart(2, '0');
 }
 
-function timerUpdate(){
+function timerUpdate() {
   const currentDate = new Date();
   const differentDate = targetDate - currentDate;
 
-  if(differentDate > 0){
+  if (differentDate > 0) {
     const { days, hours, minutes, seconds } = convertMs(differentDate);
     dataDays.textContent = formatTime(days);
     dataHours.textContent = formatTime(hours);
     dataMinutes.textContent = formatTime(minutes);
     dataSeconds.textContent = formatTime(seconds);
-    
-  } else{
+
+  } else {
     clearInterval(intervalId);
     Notiflix.Notify.failure('Please choose a date in the future');
-    btnStart.disabled = true;
+
   }
 }
 
-startBtn.addEventListener("click", startTimer);
+startBtn.addEventListener('click', startTimer);
 
-function startTimer(){
-  startBtn.disabled = false;
-  if(dataPicker.selectedDates.length > 0){
-    targetDate = new Date(dataPicker.selectedDates[0])
-    
+function startTimer() {
+  if (dataPicker.selectedDates.length > 0) {
+    targetDate = new Date(dataPicker.selectedDates[0]);
+
     timerUpdate();
     clearInterval(intervalId);
     intervalId = setInterval(timerUpdate, 1000);
-    
-    
-  }else{
-    Notiflix.Notify.warning("Choose a date");
+  } else {
+    Notiflix.Notify.warning('Choose a date');
   }
 }
-
